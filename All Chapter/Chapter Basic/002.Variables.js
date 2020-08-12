7db2d340440597a035180e528118ab022915bc25
[firstname <mdammad75@gmail>
2020-08-12 18:29:25 +0600

    short change

---
+++ All Chapter/Chapter Basic/002.Variables.js
@@ -0,0 +1,9 @@
+// Variables types
+var name = "Md Ammad Ali";
+var age = 30;
+console.log(name + "Knows javascript.");
+console.log("His age is only " + age);
+console.log(name + "is creating a javascript course for all");
+console.log("But his age is only " + age);
+console.log(9 + 9);
+console.log('9' + 9);
--- All Chapter/Chapter Basic/006.Hello.js
+++
@@ -1,5 +0,0 @@
-console.log('Hello World');
-console.log('I love Bangladesh.');
-console.log(45);
-console.log(60.567);
-console.log('My favarite number ' + 9);
